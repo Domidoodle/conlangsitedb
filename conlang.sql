52b2e389a28a1ce0ae5826d010e6d2d50631568f
Domidoodle <6635233+Domidoodle@users.noreply.github.com>
2020-11-25 08:57:24 +1300

    25/11/2020

--- conlang.sql
+++ conlang.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 5.0.3
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Oct 29, 2020 at 11:39 PM
--- Server version: 10.4.11-MariaDB
--- PHP Version: 7.4.5
+-- Generation Time: Nov 24, 2020 at 08:56 PM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.2.34
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -40,11 +40,12 @@ CREATE TABLE `conlangs` (
 --
 
 INSERT INTO `conlangs` (`id`, `name`, `name_romanised`, `script_id`, `pronunciation`) VALUES
-(1, 'dossin', 'Dossin', 1, NULL),
+(1, 'dossin', 'Dossyn', 1, ''),
 (2, 'heizi', 'Heizi', 1, NULL),
 (3, 'Proto-Tineba', NULL, NULL, NULL),
 (4, 'ᚾᛟᚱᛖᚾᛋ', 'Norens', NULL, NULL),
-(5, 'sfdfd', '', NULL, '');
+(15, 'Toki Pona', '', NULL, 'toki pona '),
+(19, 'newlanguage', '', 11, '');
 
 -- --------------------------------------------------------
 
@@ -67,7 +68,9 @@ INSERT INTO `editors` (`id`, `user_id`, `conlang_id`) VALUES
 (2, 7, 4),
 (3, 8, 2),
 (4, 6, 2),
-(8, 6, 5);
+(18, 6, 3),
+(19, 48, 15),
+(23, 49, 19);
 
 -- --------------------------------------------------------
 
@@ -91,18 +94,40 @@ CREATE TABLE `meanings` (
 
 INSERT INTO `meanings` (`id`, `word_id`, `pos`, `english`, `meaning`, `example`, `example_english`) VALUES
 (1, 1, 'noun', 'writing', 'any form of writing but usually referring to the specific dossï script', 'Tevin kaspan posonnë. Te rad «kjave» ne.  \nI saw writing on the ground. It said \"fish\".  ', ''),
-(4, 2, 'verb', 'think, imagine', NULL, NULL, NULL),
+(4, 2, 'verb', 'think', NULL, NULL, NULL),
 (27, 14, 'noun', 'water', 'yeah its water', 'Eiski vosï.\nI drink water', NULL),
 (32, 15, 'verb', 'eat, eating', NULL, NULL, NULL),
 (33, 16, 'noun', 'snow', '', NULL, NULL),
-(37, 1, 'verb', 'test', NULL, NULL, NULL),
-(38, 17, 'noun', 'oh no', 'oh no', NULL, NULL),
-(45, 40, 'adjective', 'red', '', '', NULL),
+(45, 40, 'adjective', 'red', 'fdsfdsf', 'efsdf', 'aaaa'),
 (46, 41, 'verb', 'drink, drinking', NULL, NULL, NULL),
-(47, 43, 'noun', 'dfsdfsd', NULL, NULL, NULL),
-(51, 40, NULL, NULL, NULL, NULL, NULL),
-(52, 40, NULL, NULL, NULL, NULL, NULL),
-(53, 40, NULL, NULL, NULL, NULL, NULL);
+(58, 51, 'adjective', 'blue', NULL, NULL, NULL),
+(59, 52, 'noun', 'house', 'building to live in', '', NULL),
+(60, 53, 'adjective', 'beautiful', 'good-looking or attractive', NULL, NULL),
+(61, 54, 'noun', 'person', NULL, NULL, NULL),
+(62, 55, 'noun', 'nation', 'as in nationality', NULL, NULL),
+(63, 55, 'noun', 'country', 'usually used for a nationstate', NULL, NULL),
+(64, 56, 'noun', 'blood', '', NULL, NULL),
+(65, 57, 'noun', 'air', NULL, NULL, NULL),
+(67, 58, 'pronoun', 'I', '1SG first person singular pronoun - jaz in some dialects', '', NULL),
+(68, 59, 'affix', 'diminunitive ', NULL, 'Az tevi namaucin!\nI see tiny cats!', NULL),
+(69, 60, 'noun', 'woman', NULL, NULL, NULL),
+(70, 61, 'noun', 'nature', '', NULL, NULL),
+(71, 62, 'verb', 'believe', NULL, NULL, NULL),
+(72, 66, 'verb', 'speak', '', 'Jaz jovy Dossyn\nI speak Dossyn', NULL),
+(73, 2, 'verb', 'imagine', NULL, NULL, NULL),
+(74, 68, 'noun', 'lizard', NULL, NULL, NULL),
+(75, 69, NULL, 'no', NULL, NULL, NULL),
+(76, 70, 'verb', 'hunt', NULL, NULL, NULL),
+(77, 71, 'pronoun', NULL, NULL, NULL, NULL),
+(78, 72, 'adjective', 'low', '', NULL, NULL),
+(79, 73, NULL, NULL, NULL, NULL, NULL),
+(80, 74, 'noun', 'fire', NULL, NULL, NULL),
+(81, 75, 'adjective', 'happy', NULL, NULL, NULL),
+(82, 76, 'noun', 'ground', NULL, NULL, NULL),
+(83, 77, 'verb', 'labour', NULL, NULL, NULL),
+(84, 78, 'verb', 'see', NULL, NULL, NULL),
+(85, 79, 'noun', 'language', NULL, NULL, NULL),
+(86, 80, 'noun', 'neword', NULL, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -121,7 +146,8 @@ CREATE TABLE `scripts` (
 --
 
 INSERT INTO `scripts` (`id`, `name`, `user_id`) VALUES
-(1, 'kaspak', 6);
+(1, 'kaspak', 6),
+(11, 'The Standard Galactic Alphabet', 49);
 
 -- --------------------------------------------------------
 
@@ -144,12 +170,8 @@ INSERT INTO `users` (`id`, `name`, `pwd`, `email`) VALUES
 (6, 'domidoodle', '$2y$10$JbpABWC38lmx4sGzatcijONCUPHJTshiq9lmkDmZiqhobTqWr1iNy', 'c1ae51d2958b05e0c0681c7e6416c64c07cc0bcd36697a115c0955ca33c2977e'),
 (7, 'cawlo', '$2y$10$ukZ/bSWn12jYhBmEspjE/OcVPcDFS2Q0iNZzemoOdFbTJTnGXGc3S', '3fdc2628afd5c1494ff3080de4e98b757689e108c3504bcd16fbf84e0a9b7bda'),
 (8, 'Jim Saladears', '$2y$10$nm7e93WbtSPKxxXsm15m/.hMCher8jMCcRYBA97bXFNPe24J7d1US', '8fc1ba7ae25e86e9936f664850be6b7315c7403274444b7f12bbd994ca5f3b7e'),
-(18, 'bgfdsyagfbads', '$2y$10$QGF1FCU9FPu5GBU0BiG/cuKK18SlfrFjrZG.WpOI6fhiuD5uggGm2', '3c78a170a0cff3f552df4d03dd8c5d6e3d17340965b066a2519efdd0433e8610'),
-(19, 'bgfdsyagfbads', '$2y$10$oZmmcmHbXD859OUFNlmUMO5DNArrPUHNkdFk9Io5csVmC13t42Iru', '3c78a170a0cff3f552df4d03dd8c5d6e3d17340965b066a2519efdd0433e8610'),
-(20, 'bgfdsyagfbads', '$2y$10$z5ChPm6wG950pakmqCXB4eciqhxlRgZabsTTai3qq7jOszTfdFnq6', '3c78a170a0cff3f552df4d03dd8c5d6e3d17340965b066a2519efdd0433e8610'),
-(21, 'bgfdsyagfbads', '$2y$10$oL6lbSv7Kf/4khLO0PrQiuLFLiNFVjgjslb.rGLktfv61PpaX8FxW', '3c78a170a0cff3f552df4d03dd8c5d6e3d17340965b066a2519efdd0433e8610'),
-(22, 'bgfdsyagfbads', '$2y$10$Hxc.qu8gWRvJ7MACC6x8/ugLCMhhb/9CAPEQbjtZmralZlR2ctaY2', '3c78a170a0cff3f552df4d03dd8c5d6e3d17340965b066a2519efdd0433e8610'),
-(23, 'b', '$2y$10$wcObV/hxWVNW3eXPW8a0xOU5eGxiTQQljc1UrPbCuqE0Oq8tvE7em', 'ca978112ca1bbdcafac231b39a23dc4da786eff8147c4e72b9807785afee48bb');
+(48, 'misali', '$2y$10$pRpV77P8M7G48OcndTmFn.uH6kowVIPjZWl/9xevGbezTTGI7kqF2', '72bfc6ac2affc80e05fd1737906ccb21b660638f2ecb05dff5535bacac4c065b'),
+(49, 'newuser', '$2y$10$fcK84yyMH4D9kkr1uE87LOrRnev/Xrg8GP5i/35NJcG6Wi9gfRlUO', '3e43b8d310657a395a4a6e27d47424ce712468aa040944f952ae2a1bc2a2b1ae');
 
 -- --------------------------------------------------------
 
@@ -180,9 +202,34 @@ INSERT INTO `words` (`id`, `conlang_id`, `name`, `name_romanised`, `pronunciatio
 (37, 3, 'proto-word', NULL, 'proto-word but how to pronounce it', NULL),
 (40, 1, 'cauz', 'cauz', 'cauz', NULL),
 (41, 1, 'jouvo', 'jouvo', 'jouvo', NULL),
-(42, 1, NULL, NULL, NULL, NULL),
-(43, 1, 'kdfndsjfnd', NULL, NULL, NULL),
-(44, 1, 'dvdxvdfxvdfxv', NULL, NULL, NULL);
+(51, 1, 'hins', 'hins', 'hɪns', NULL),
+(52, 1, 'kaisi', 'kaisi', 'kɐɪsɪ', NULL),
+(53, 1, 'karoï', 'karoy', 'kɐɹɔy', NULL),
+(54, 1, 'kassi', 'kassi', 'kɐssɪ', NULL),
+(55, 1, 'kassifdaus', 'kassifdaus', 'kɐssɪfdɐʊs', NULL),
+(56, 1, 'kazt', 'kazt', 'kɐzt', NULL),
+(57, 1, 'luha', 'luha', 'lʊhɐ', NULL),
+(58, 1, 'az', 'az', 'ɐz', NULL),
+(59, 1, 'cin', 'cin', 'ʃɪn', NULL),
+(60, 1, 'ecri', 'ecri', 'ɛʃɹɪ', NULL),
+(61, 1, 'cvates', 'cvates', 'ʃvɐtɛs', NULL),
+(62, 1, 'daizi', 'daizi', 'dɐɪzɪ', NULL),
+(63, 1, NULL, NULL, NULL, NULL),
+(64, 1, NULL, NULL, NULL, NULL),
+(66, 1, 'jov', 'jov', 'jɔv', NULL),
+(68, 15, 'akesi', NULL, 'akesi', NULL),
+(69, 15, 'ala', NULL, 'ala', NULL),
+(70, 15, 'alasa', NULL, 'alasa', NULL),
+(71, 15, 'ale', '', 'ale', NULL),
+(72, 15, 'anpa', NULL, 'anpa', NULL),
+(73, 15, 'ante', '', 'ante', NULL),
+(74, 1, 'fjon', 'fjon', 'fʲɔn', NULL),
+(75, 1, 'kari', 'kari', 'kɐɹɪ', NULL),
+(76, 1, 'poso', 'poso', 'pɔsɔ', NULL),
+(77, 1, 'toco', 'toco', 'tɔʃɔ', NULL),
+(78, 1, 'tev', 'tev', 'tɛv', NULL),
+(79, 1, 'voc', 'voc', 'vɔʃ', NULL),
+(80, 19, 'newword', 'neword', NULL, NULL);
 
 --
 -- Indexes for dumped tables
@@ -239,37 +286,37 @@ ALTER TABLE `words`
 -- AUTO_INCREMENT for table `conlangs`
 --
 ALTER TABLE `conlangs`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- AUTO_INCREMENT for table `editors`
 --
 ALTER TABLE `editors`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 
 --
 -- AUTO_INCREMENT for table `meanings`
 --
 ALTER TABLE `meanings`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=54;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=88;
 
 --
 -- AUTO_INCREMENT for table `scripts`
 --
 ALTER TABLE `scripts`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=50;
 
 --
 -- AUTO_INCREMENT for table `words`
 --
 ALTER TABLE `words`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=45;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=81;
 
 --
 -- Constraints for dumped tables
